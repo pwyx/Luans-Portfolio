09cc0f75d52eaa8eff639e8322007bef3cdff28e
Peter Stockwell <peter.stockwell@student.adelaide.edu.au>
2015-10-09 04:00:02 +1030

    contact page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,34 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-  //
+  // change label color on form focus
+  $('form label textarea, form label input').focus(function() {
+    $(this).siblings('span').addClass('input-focus');
+  }).blur(function() {
+    $('span').removeClass('input-focus');
+  });
 
 });
+
+$(window).load(function($){
+  header_scroll();
+});
+
+function header_scroll(){
+  if( $(window).width() > 767 && !( $('html').hasClass('ie9') ) ){
+  
+    var windowHeight = $(window).height();
+    var sidebarHeight = $('.sidebar').height();
+    var documentHeight = $(document).height();
+
+    // $('.sidebar').css({ 'min-height' : windowHeight });
+
+    $(window).scroll(function(){
+      if($(window).scrollTop() < documentHeight - sidebarHeight){
+        $('.sidebar').css({ 'position' : 'fixed', 'top' : '0' });
+      } else {
+        $('.sidebar').css({ 'position' : 'absolute', 'top' : documentHeight - sidebarHeight });
+      }
+    });
+
+  }
+}
