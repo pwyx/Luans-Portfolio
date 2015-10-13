522009dafbf1bc0e288fb6aeb4ef6d13fb93290e
Peter Stockwell <peter.stockwell@student.adelaide.edu.au>
2015-10-14 02:24:58 +1030

    sidebar scrolling

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // change label color on form focus
   $('form label textarea, form label input').focus(function() {
     $(this).siblings('span').addClass('input-focus');
   }).blur(function() {
@@ -12,21 +13,38 @@ $(window).load(function($){
   header_scroll();
 });
 
+// function header_scroll(){
+//   var innerHeight = $('.sidebar-menu').height();
+//   var documentHeight = $(document).height();
+
+//   // $('.sidebar').css({ 'min-height' : $(window).height() });
+
+//   $(window).scroll(function(){
+//     if( $(window).scrollTop() < documentHeight - innerHeight ){
+//       $('.sidebar-menu').css({ 'position' : 'fixed', 'top' : '0' });
+//     } else {
+//       $('.sidebar-menu').css({ 'position' : 'absolute', 'top' : documentHeight - innerHeight });
+//     }
+//   });
+
+// }
 
 function header_scroll(){
   if( $(window).width() > 767 && !( $('html').hasClass('ie9') ) ){
-  
-    var windowHeight = $(window).height();
-    var sidebarHeight = $('.sidebar').height();
+
+    // $('.sidebar').css({ 'min-height' : $(window).height() });
+
+    // if( $('.sidebar .sidebar-menu').length == 0 )
+    //   $('.sidebar').wrapInner("<div class='sidebar-menu'></div>");
+
+    var innerHeight = $('.sidebar-menu').height();
     var documentHeight = $(document).height();
 
-    // $('.sidebar').css({ 'min-height' : windowHeight });
-
     $(window).scroll(function(){
-      if($(window).scrollTop() < documentHeight - sidebarHeight){
+      if( $(window).scrollTop() < documentHeight - innerHeight ){
         $('.sidebar').css({ 'position' : 'fixed', 'top' : '0' });
       } else {
-        $('.sidebar').css({ 'position' : 'absolute', 'top' : documentHeight - sidebarHeight });
+        $('.sidebar').css({ 'position' : 'absolute', 'top' : documentHeight - innerHeight });
       }
     });
 
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,18 +15,20 @@ $(window).load(function($){
 
 function header_scroll(){
   if( $(window).width() > 767 && !( $('html').hasClass('ie9') ) ){
-  
-    var windowHeight = $(window).height();
-    var sidebarHeight = $('.sidebar').height();
+
+    // $('.sidebar').css({ 'min-height' : $(window).height() });
+
+    // if( $('.sidebar .sidebar-menu').length == 0 )
+    //   $('.sidebar').wrapInner("<div class='sidebar-menu'></div>");
+
+    var innerHeight = $('.sidebar-menu').height();
     var documentHeight = $(document).height();
 
-    // $('.sidebar').css({ 'min-height' : windowHeight });
-
     $(window).scroll(function(){
-      if($(window).scrollTop() < documentHeight - sidebarHeight){
+      if( $(window).scrollTop() < documentHeight - innerHeight ){
         $('.sidebar').css({ 'position' : 'fixed', 'top' : '0' });
       } else {
-        $('.sidebar').css({ 'position' : 'absolute', 'top' : documentHeight - sidebarHeight });
+        $('.sidebar').css({ 'position' : 'absolute', 'top' : documentHeight - innerHeight });
       }
     });
 
